d98bdeb0441eccc42f5bd64d1c36f1a382275e95
suportpixelrise-ui <suport.pixelrise@gmail.com>
2025-11-07 10:57:41 -0300

    Update route.ts

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -1,9 +1,8 @@
-import { NextRequest, NextResponse } from 'next/server'
-import Stripe from 'stripe'
+import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2024-06-20',
-})
+});
 
 export async function POST(request: NextRequest) {
   try {

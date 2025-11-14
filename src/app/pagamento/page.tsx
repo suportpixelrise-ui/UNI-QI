6244d95545dbf9dbe2dbc0756615f8dae391ea9b
suportpixelrise-ui <suport.pixelrise@gmail.com>
2025-11-14 19:22:40 -0300

    Update page.tsx

--- src/app/pagamento/page.tsx
+++ src/app/pagamento/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import PagamentoClient from "./PagamentoClient";
 
-export default function PagamentoPage() {
-  return <PagamentoClient />;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <PagamentoClient />
+    </Suspense>
+  );
 }
